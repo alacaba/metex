70f1dca9a3c005f30e39c13d2626224f046ddcc5
alacaba <alacaba@fastmail.com>
2019-03-25 17:06:43 +0800

    Create coordinator to get worker response

--- lib/metex.ex
+++ lib/metex.ex
@@ -15,4 +15,16 @@ defmodule Metex do
   def hello do
     :world
   end
+
+  def temperatures_of(cities) do
+    coordinator_pid =
+      spawn(Metex.Coordinator, :loop, [[], Enum.count(cities)])
+
+    apply_to_city = fn city ->
+      worker_pid = spawn(Metex.Worker, :loop, [])
+      send(worker_pid, {coordinator_pid, city})
+    end
+
+    cities |> Enum.each(apply_to_city)
+  end
 end
