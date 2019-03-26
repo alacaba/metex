001a9ca9976dc24afa62b442d38354e08279fd2d
alacaba <alacaba@fastmail.com>
2019-03-26 10:36:33 +0800

    Initial GenServer setup

--- lib/worker.ex
+++ lib/worker.ex
@@ -1,42 +1,39 @@
 defmodule Metex.Worker do
-  def ping do
-    receive do
-      _ ->
-        IO.puts "pong"
-    end
-    ping
+  use GenServer
+
+  # Client API
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts)
   end
 
-  def pong do
-    receive do
-      _ ->
-        IO.puts "ping"
-    end
-    pong
+  def get_temperature(pid, location) do
+    GenServer.call(pid, {:location, location})
   end
 
-  def loop do
-    receive do
-      {sender_pid, location} ->
-        send(sender_pid, {:ok, temperature_of(location)})
-      _ ->
-        IO.puts "don't know how to process this message"
-    end
-    loop
+  # Server Callbacks
+
+  def init(:ok) do
+    {:ok, %{}}
   end
+
+  def handle_call({:location, location}, _from, stats) do
+    case temperature_of(location) do
+      {:ok, temp} ->
+        new_stats = update_stats(stats, location)
+        {:reply, "#{temp}°C", new_stats}
+      _ ->
+        {:reply, :error, stats}
+    end
+  end
+
+  # Helper Functions
 
   def temperature_of(location) do
-    result = location
-             |> url_for()
-             |> HTTPoison.get()
-             |> parse_response()
-
-    case result do
-      {:ok, temp} ->
-        "#{location}: #{temp} °C"
-      :error ->
-        "#{location} not found"
-    end
+    location
+    |> url_for()
+    |> HTTPoison.get()
+    |> parse_response()
   end
 
   defp url_for(location) do
@@ -45,23 +42,30 @@ defmodule Metex.Worker do
   end
 
   defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
-    body |> JSON.decode() |> compute_temperature()
+    body
+    |> JSON.decode()
+    |> compute_temperature()
   end
 
-  defp parse_response(_) do
-    :error
-  end
+  defp parse_response(_), do: :error
 
   defp compute_temperature({:ok, json}) do
     temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
     {:ok, temp}
   end
 
-  defp compute_temperature({:error, _}) do
-    :error
-  end
+  defp compute_temperature({:error, _}), do: :error
 
   defp apikey do
     "686e8cca60baeb479503d5d87cdfbbf0"
+  end
+
+  defp update_stats(old_stats, location) do
+    case Map.has_key?(old_stats, location) do
+      true ->
+        Map.update!(old_stats, location, &(&1 + 1))
+      false ->
+        Map.put_new(old_stats, location, 1)
+    end
   end
 end
